ded0ef9e17eeebe5f9143e5db911b6837c8846fc
administrator <dongzhongyang@lanlingdai.net>
2019-01-08 10:34:24 +0800

    update

---
+++ app/src/main/java/com/qubuxing/qbx/http/beans/DeviceInfo.kt
@@ -0,0 +1,8 @@
+package com.qubuxing.qbx.http.beans
+
+class DeviceInfo {
+    var deviceOwner =""
+    var deviceBrand = ""
+    var deviceImei = ""
+    var osVer =""
+}
---
+++ app/src/main/java/com/qubuxing/qbx/http/beans/KFentity.kt
@@ -0,0 +1,7 @@
+package com.qubuxing.qbx.http.beans
+
+class KFentity {
+    var email = ""
+    var phone = ""
+    var name = ""
+}
